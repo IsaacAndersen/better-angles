50f8ab308eb9d014b2ec0401edec9896cf47db4a
isaac <isaaclandersen@gmail.com>
2022-08-14 15:20:58 -0400

    move some stuff

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,107 +2,28 @@ import * as THREE from 'three';
 import { Vector3 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GUI } from 'dat.gui';
-
-// TODO: does it make the most sense to write this as a class?
-const screenshot = require('../images/m2.jpeg');
-
-const image = document.createElement("img");
-image.id = "myImage";
-image.style.display = "none";
-document.body.appendChild(image);
+import ImageRotator from './imageRotator';
 
 
+// Setup THREE.js scene
 const renderer = new THREE.WebGLRenderer({
     antialias: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
-
 document.body.appendChild( renderer.domElement );
-
 const scene = new THREE.Scene();
-
 scene.add(new THREE.AmbientLight(0xffffff, 0.5));
+
+
 // scene.background = new THREE.Color(0x00ffff);
 
-type PlaneParams = {
-    rotation: THREE.Euler;
-    color: THREE.Color;
-};
-
-const makeRing = (params: PlaneParams) => {
-    const geometry = new THREE.TorusGeometry(5, 0.01, 64, 64);
-    const material = new THREE.MeshBasicMaterial( { color: params.color, side: THREE.DoubleSide } );
-    const mesh = new THREE.Mesh( geometry, material );
-    mesh.rotation.copy(params.rotation);
-    return mesh;
-}
-
-const texture = new THREE.Texture(image);
-const textureBack = new THREE.Texture(image);
-textureBack.flipY = false;
-// Display an image on a plane
-const makeImage = (params: PlaneParams) => {
-    const geometry = new THREE.PlaneGeometry(10, 10);
-    const geometryBack = new THREE.PlaneGeometry(10, 10);
-
-    geometryBack.applyMatrix4( new THREE.Matrix4().makeRotationY( Math.PI ) );
-
-    const material = new THREE.MeshBasicMaterial({
-        map: texture,
-        side: THREE.FrontSide,
-    });
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.rotation.copy(params.rotation);
-
-    const materialBack = new THREE.MeshBasicMaterial({
-        map: textureBack,
-        side: THREE.FrontSide,
-        transparent: true,
-        opacity: 0.5,
-    });
-    materialBack
-
-    const meshBack = new THREE.Mesh(geometryBack, materialBack);
-    meshBack.rotation.copy(params.rotation);
-    meshBack.name = "back";
-
-    const image = new THREE.Group();
-    image.add(mesh);
-    image.add(meshBack);
-
-    return image;
-};
+// Definte types.
 
 
-// A materal that displays the outline
-const outlineMaterial = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    side: THREE.DoubleSide,
-    transparent: true,
-    opacity: 0.05,
-});
 
-const compassRingParams: PlaneParams[] = [
-    // Green => Rotation about X axis
-    {rotation: new THREE.Euler(0, Math.PI / 2, 0, 'XYZ'), color: new THREE.Color(0x00ff00)},
-    // Blue  => Rotation about Y axis
-    {rotation: new THREE.Euler(0, 0, Math.PI / 2, 'XYZ'), color: new THREE.Color(0x0000ff)},
-    // Red   => Rotation about Z axis
-    {rotation: new THREE.Euler(Math.PI / 2, 0, 0, 'XYZ'), color: new THREE.Color(0xff0000)},
-];
 
-compassRingParams
-    .map(makeRing)
-    .forEach(ring => { scene.add(ring) });
+const imageRotator = new ImageRotator();
 
-const images = compassRingParams
-    .slice(0, 1)
-    .map(makeImage);
-    
-
-scene.add(...images);
-
-const ambientLight = new THREE.AmbientLight( 0xffffff, 0.5 );
-scene.add( ambientLight );
+scene.add(imageRotator.threeObject);
 
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
 const controls = new OrbitControls( camera, renderer.domElement );
@@ -188,11 +109,11 @@ export const animate = () => {
         anchor.click();
         window.URL.revokeObjectURL(url);
         var reader = new FileReader();
-        var imgtag = document.getElementById("myImage") as HTMLImageElement;
+        var imgtag = imageRotator.image;
 
         reader.onload = (event: ProgressEvent<FileReader>) => {
             imgtag.src = event.target.result as string;
-            images[0].children.forEach((child, _) => {
+            imageRotator.imagePlane.children.forEach((child, _) => {
                 const mesh = (<THREE.Mesh> child);
                 // mesh.geometry.scale(ratio, 1, 1);
 
@@ -200,8 +121,8 @@ export const animate = () => {
                 const newTexture = new THREE.Texture(imgtag);
     
                 if (child.name === "back") {
-                    texture.center = new THREE.Vector2(0.5, 0.5);
-                    texture.rotation = Math.PI;
+                    newTexture.center = new THREE.Vector2(0.5, 0.5);
+                    newTexture.rotation = Math.PI;
                 }
     
                 material.map = newTexture;
@@ -238,10 +159,8 @@ window.addEventListener("keydown", (event) => {
 window.addEventListener( 'resize', onWindowResize, false );
 
 function onWindowResize(){
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
     renderer.setSize( window.innerWidth, window.innerHeight );
 
 }
